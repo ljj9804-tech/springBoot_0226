f9e1f728176cb929ba8c907969fcafa54749589e
leejinju <ljj9804@gmail.com>
2026-03-05 16:11:23 +0900

    20260305_수업

--- src/main/java/com/busanit501/springboot_0226/dto/PageRequestDTO.java
+++ src/main/java/com/busanit501/springboot_0226/dto/PageRequestDTO.java
@@ -52,7 +52,7 @@ public class PageRequestDTO {
     // 변경2,===============================================================
 
     public String getLink() {
-        if(link == null && link.isEmpty()) {
+        if(link == null || link.isEmpty()) {
             // 기본 : String, 불변, 객체를 새로 생성할 때마다, 새로운 메모리를 사용하고,
             // StringBuilder, 기존 메모리에(변경없이) 작업 및 사용함, 메모리 절약 효과. 그래서, 사용함.
             StringBuilder builder = new StringBuilder();
